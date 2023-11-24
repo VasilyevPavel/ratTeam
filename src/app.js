4c1fb5a639c3bc443237098eb6e3cd0bb23278cf
Pavel Vasilyev <vasilps1989@mail.ru>
2023-11-24 05:45:39 +0000

    service added

--- src/app.js
+++ src/app.js
@@ -1,44 +1,32 @@
 require('dotenv').config();
-const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
-const expressSession = require('express-session');
-const FileStore = require('session-file-store')(expressSession);
+const cookieParser = require('cookie-parser');
 
-const apiRouter = require('./routers/api.router');
+const apiRouter = require('./src/routers/api.router');
+const errorMiddleware = require('./src/middlewares/error-middlewares');
 
-const { PORT, SECRET_KEY_SESSION } = process.env;
-
+const { PORT } = process.env;
 const corsOptions = {
-  origin: [process.env.CORS_ORIGIN],
+  origin: ['http://localhost:3000'],
   credentials: true,
 };
-
-const sessionConfig = {
-  store: new FileStore(),
-  secret: SECRET_KEY_SESSION,
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 100 * 60 * 1000,
-    httpOnly: true,
-  },
-};
-
 const app = express();
 
-app.use(expressSession(sessionConfig));
-
-app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(process.cwd(), 'storage')));
+app.use('/api', apiRouter);
+app.use(errorMiddleware);
 
-// app.use('/api', apiRouter);
-
-app.listen(PORT, () => {
-  console.log(`Server has been started on ${PORT}`);
-});
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server has been started on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
--- src/routers/api.router.js
+++ src/routers/api.router.js
@@ -1,1 +1,5 @@
+const authRouter = require('./auth.router');
+
 const router = require('express').Router();
+
+module.exports = router.use('/auth', authRouter);
