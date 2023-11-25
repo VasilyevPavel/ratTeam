e549bdf072eddc80086e2b1aa278339537ad42c7
VasilyevPavel <iam100500@ya.ru>
2023-11-26 00:28:21 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -33,7 +33,6 @@ app.use((req, res, next) => {
   }
 
   res.header('Access-Control-Allow-Credentials', true);
-
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
