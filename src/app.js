744e0ecbdecf87fa0cd53907bdc6da993b4c8225
VasilyevPavel <iam100500@ya.ru>
2023-11-26 00:33:17 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -9,39 +9,18 @@ const errorMiddleware = require('./middlewares/error-middlewares');
 
 const { PORT, CORS_ORIGIN } = process.env;
 
-// const corsOptions = {
-//   origin: ['https://xll792-3000.csb.app', 'http://localhost:3000'],
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: ['https://xll792-3000.csb.app', 'http://localhost:3000'],
+  credentials: true,
+};
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use('/api', apiRouter);
 app.use(errorMiddleware);
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    'https://xll792-3000.csb.app',
-    'http://localhost:3000',
-  ];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 const start = async () => {
   try {
